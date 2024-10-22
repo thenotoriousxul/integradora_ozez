de0ac043a70db30224a54cae469193fba1325b45
ZeckteK <24170023@uttcampus.edu.mx>
2024-10-21 20:32:54 -0600

    Agregue los modelos de la bd

---
+++ app/Models/ProviderDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProviderDetail extends Model
+{
+    use HasFactory;
+    protected $table = 'providers_details';
+}
